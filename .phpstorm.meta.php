abebc58267ea26bbc3834c1a55342ddd5f931c85
zmy <zhengmingyang1996@gmail.com>
2019-09-08 21:01:11 +0800

    升级 Laravel 框架至 5.8.32

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-08-20 15:16:14.
+    * Generated on 2019-09-08 20:59:24.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
